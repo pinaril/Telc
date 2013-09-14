2314a391608d4144dae01445a6fdb2e1d2d3fedc
hongxukangkang <zhaokangk@sina.com>
2013-09-14 21:00:03 +0800

    赵康提交

---
+++ SmartMemo/src/com/telc/ui/main/AnimationActivity.java
@@ -0,0 +1,44 @@
+package com.telc.ui.main;
+
+import com.telc.smartmemo.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+
+public class AnimationActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_animation);
+
+		AlphaAnimation aa = new AlphaAnimation(0.1f, 1.0f);
+		aa.setDuration(6000); // 
+
+		this.findViewById(R.id.imageView1).startAnimation(aa);// 
+		aa.setAnimationListener(new AnimationListener() {
+			@Override
+			public void onAnimationEnd(Animation animation) {
+				Intent intent = new Intent(AnimationActivity.this,
+						SlidingActivity.class);
+				startActivity(intent);
+				AnimationActivity.this.finish();
+			}
+
+			@Override
+			public void onAnimationRepeat(Animation animation) {
+				
+			}
+
+			@Override
+			public void onAnimationStart(Animation animation) {
+				
+			}
+		});
+	}
+}
