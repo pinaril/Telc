b3888e4685d732fb0f81fe5f522e4aa598d51391
evil4t <wakeupevil@gmail.com>
2013-09-30 11:23:20 +0800

    //Menu选中效果

--- SmartMemo/src/com/telc/ui/main/viewModel/MenuFragment.java
+++ SmartMemo/src/com/telc/ui/main/viewModel/MenuFragment.java
@@ -11,6 +11,7 @@ import android.app.FragmentManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,8 +20,8 @@ import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 public class MenuFragment extends Fragment {
-	int index=0;
-	
+	int index = 0;
+
 	TextView textUserInfo;
 	TextView textFinished;
 	TextView textUnfinish;
@@ -30,141 +31,230 @@ public class MenuFragment extends Fragment {
 	TextView textReturn;
 	ContentFragment contentFragment;
 	UnfinishFragment unfinishFragment;
-	
+	Drawable drawable;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
-        super.onCreate(savedInstanceState);
-       
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+
 	}
-	
-		@Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState){	
-		View view=inflater.inflate(R.layout.activity_menu, null);
-        textUserInfo=(TextView)view.findViewById(R.id.txt_userinfo);
-        textFinished=(TextView) view.findViewById(R.id.txt_finish);
-        textUnfinish=(TextView) view.findViewById(R.id.txt_unfinish);
-        textAbout=(TextView) view.findViewById(R.id.txt_about);
-        textSetting=(TextView) view.findViewById(R.id.txt_setting);
-        textExit=(TextView) view.findViewById(R.id.txt_exit);
-        textReturn=(TextView) view.findViewById(R.id.txt_return);
-        
-        textUserInfo.setOnClickListener(new OnClickListener() {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.activity_menu, null);
+		textUserInfo = (TextView) view.findViewById(R.id.txt_userinfo);
+		textFinished = (TextView) view.findViewById(R.id.txt_finish);
+		textUnfinish = (TextView) view.findViewById(R.id.txt_unfinish);
+		textAbout = (TextView) view.findViewById(R.id.txt_about);
+		textSetting = (TextView) view.findViewById(R.id.txt_setting);
+		textExit = (TextView) view.findViewById(R.id.txt_exit);
+		textReturn = (TextView) view.findViewById(R.id.txt_return);
+
+		textUserInfo.setOnClickListener(new OnClickListener() {
+			@SuppressWarnings("deprecation")
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				if(index==1)
-					 ((SlidingActivity)getActivity()).getSlidingMenu().toggle();
-				index=1;
-				FragmentManager fm = ((SlidingActivity)getActivity()).getFragmentManager();
+				if (index == 1)
+					((SlidingActivity) getActivity()).getSlidingMenu().toggle();
+				index = 1;
+
+				drawable = getResources().getDrawable(R.drawable.bg_press);
+				textUserInfo.setBackgroundDrawable(drawable);
+				textFinished.setBackgroundDrawable(null);
+				textUnfinish.setBackgroundDrawable(null);
+				textSetting.setBackgroundDrawable(null);
+				textAbout.setBackgroundDrawable(null);
+				textReturn.setBackgroundDrawable(null);
+				textExit.setBackgroundDrawable(null);
+
+				FragmentManager fm = ((SlidingActivity) getActivity())
+						.getFragmentManager();
 				/**
-				 * @parma  replace（被替换的layout，新的fragment）
+				 * @parma replace（被替换的layout，新的fragment）
 				 */
-				fm.beginTransaction().replace(R.id.unfinish, new PersonalInfoFragment())
-				.commit();
-				((SlidingActivity)getActivity()).getSlidingMenu().showContent();
+				fm.beginTransaction()
+						.replace(R.id.unfinish, new PersonalInfoFragment())
+						.commit();
+				((SlidingActivity) getActivity()).getSlidingMenu()
+						.showContent();
 			}
 		});
-        
-        textUnfinish.setOnClickListener(new OnClickListener() {
-			
+
+		textUnfinish.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
-				if(index==2)
-					((SlidingActivity)getActivity()).getSlidingMenu().toggle();
-				index=2;
-				FragmentManager fm = ((SlidingActivity)getActivity()).getFragmentManager();
+				if (index == 2)
+					((SlidingActivity) getActivity()).getSlidingMenu().toggle();
+				index = 2;
+				FragmentManager fm = ((SlidingActivity) getActivity())
+						.getFragmentManager();
+
+				drawable = getResources().getDrawable(R.drawable.bg_press);
+				textUserInfo.setBackgroundDrawable(null);
+				textFinished.setBackgroundDrawable(null);
+				textUnfinish.setBackgroundDrawable(drawable);
+				textSetting.setBackgroundDrawable(null);
+				textAbout.setBackgroundDrawable(null);
+				textReturn.setBackgroundDrawable(null);
+				textExit.setBackgroundDrawable(null);
+
 				/**
-				 * @parma  replace（被替换的layout，新的fragment）
+				 * @parma replace（被替换的layout，新的fragment）
 				 */
-//				fm.beginTransaction().replace(R.id.unfinish, unfinishFragment == null ?new UnfinishFragment():unfinishFragment )
-				fm.beginTransaction().replace(R.id.unfinish, new UnfinishFragment())
-				.commit();
-				((SlidingActivity)getActivity()).getSlidingMenu().showContent();
-				
+				// fm.beginTransaction().replace(R.id.unfinish, unfinishFragment
+				// == null ?new UnfinishFragment():unfinishFragment )
+				fm.beginTransaction()
+						.replace(R.id.unfinish, new UnfinishFragment())
+						.commit();
+				((SlidingActivity) getActivity()).getSlidingMenu()
+						.showContent();
+
 			}
 		});
-        
-        
-        textFinished.setOnClickListener(new OnClickListener() {
-			
+
+		textFinished.setOnClickListener(new OnClickListener() {
+
+			@SuppressWarnings("deprecation")
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-//				if(index==2)
-//					 ((SlidingActivity)getActivity()).getSlidingMenu().toggle();
-//				index=2;
-//				FragmentManager fm = ((SlidingActivity)getActivity()).getFragmentManager();
-//				fm.beginTransaction().replace(R.id.content, realtimeFragment == null ?new RealtimeMemoActivity():realtimeFragment)
-//				.addToBackStack(null)
-//				.commit();
-//				((SlidingActivity)getActivity()).getSlidingMenu().showContent();
+				// if(index==2)
+				// ((SlidingActivity)getActivity()).getSlidingMenu().toggle();
+				// index=2;
+				// FragmentManager fm =
+				// ((SlidingActivity)getActivity()).getFragmentManager();
+				// fm.beginTransaction().replace(R.id.content, realtimeFragment
+				// == null ?new RealtimeMemoActivity():realtimeFragment)
+				// .addToBackStack(null)
+				// .commit();
+				drawable = getResources().getDrawable(R.drawable.bg_press);
+				textUserInfo.setBackgroundDrawable(null);
+				textFinished.setBackgroundDrawable(drawable);
+				textUnfinish.setBackgroundDrawable(null);
+				textSetting.setBackgroundDrawable(null);
+				textAbout.setBackgroundDrawable(null);
+				textReturn.setBackgroundDrawable(null);
+				textExit.setBackgroundDrawable(null);
+
+				// ((SlidingActivity)getActivity()).getSlidingMenu().showContent();
 			}
 		});
-        
-        
-        textReturn.setOnClickListener(new OnClickListener() {
-			
+
+		textReturn.setOnClickListener(new OnClickListener() {
+
+			@SuppressWarnings("deprecation")
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Editor edit=((SlidingActivity)getActivity()).sp.edit();
+				Editor edit = ((SlidingActivity) getActivity()).sp.edit();
 				edit.putBoolean("login_in", false);
 				edit.commit();
-				Intent intent=new Intent(getActivity(),LoginAndRegisterActivity.class);
+
+				drawable = getResources().getDrawable(R.drawable.bg_press);
+				textUserInfo.setBackgroundDrawable(null);
+				textFinished.setBackgroundDrawable(null);
+				textUnfinish.setBackgroundDrawable(null);
+				textSetting.setBackgroundDrawable(null);
+				textAbout.setBackgroundDrawable(null);
+				textReturn.setBackgroundDrawable(drawable);
+				textExit.setBackgroundDrawable(null);
+
+				Intent intent = new Intent(getActivity(),
+						LoginAndRegisterActivity.class);
 				startActivity(intent);
 				getActivity().finish();
 			}
 		});
-        
-        textExit.setOnClickListener(new OnClickListener() {
-			
+
+		textExit.setOnClickListener(new OnClickListener() {
+
+			@SuppressWarnings("deprecation")
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
+				drawable = getResources().getDrawable(R.drawable.bg_press);
+				textExit.setBackgroundDrawable(drawable);
 				getActivity().finish();
 			}
 		});
-        
-        return view;
-        
-        }
+
+		textSetting.setOnClickListener(new OnClickListener() {
+
+			@SuppressWarnings("deprecation")
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				drawable = getResources().getDrawable(R.drawable.bg_press);
+				textUserInfo.setBackgroundDrawable(null);
+				textFinished.setBackgroundDrawable(null);
+				textUnfinish.setBackgroundDrawable(null);
+				textSetting.setBackgroundDrawable(drawable);
+				textAbout.setBackgroundDrawable(null);
+				textReturn.setBackgroundDrawable(null);
+				textExit.setBackgroundDrawable(null);
+			}
+		});
+
+		textAbout.setOnClickListener(new OnClickListener() {
+
+			@SuppressWarnings("deprecation")
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				drawable = getResources().getDrawable(R.drawable.bg_press);
+				textUserInfo.setBackgroundDrawable(null);
+				textFinished.setBackgroundDrawable(null);
+				textUnfinish.setBackgroundDrawable(null);
+				textSetting.setBackgroundDrawable(null);
+				textAbout.setBackgroundDrawable(drawable);
+				textReturn.setBackgroundDrawable(null);
+				textExit.setBackgroundDrawable(null);
+			}
+		});
+
+		return view;
+
+	}
+
 	@Override
-    public void onDestroy() {
+	public void onDestroy() {
 
-        super.onDestroy();
-    }
+		super.onDestroy();
+	}
 
-    @Override
-    public void onDetach() {
-       
-        super.onDetach();
-    }
+	@Override
+	public void onDetach() {
 
-    @Override
-    public void onPause() {
-     
-        super.onPause();
-    }
+		super.onDetach();
+	}
 
-    @Override
-    public void onResume() {
-        
-        super.onResume();
-    }
+	@Override
+	public void onPause() {
 
-    @Override
-    public void onStart() {
-       
-        super.onStart();
-    }
+		super.onPause();
+	}
 
-    @Override
-    public void onStop() {
-      
-        super.onStop();
-    }
-	
-	
+	@Override
+	public void onResume() {
+
+		super.onResume();
+	}
+
+	@Override
+	public void onStart() {
+
+		super.onStart();
+	}
+
+	@Override
+	public void onStop() {
+
+		super.onStop();
+	}
+
 }
