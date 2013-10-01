946ac78ea5b67587b27f60d1dc96c074c9871bb4
wwb443872831 <wwb999@vip.qq.com>
2013-10-01 11:34:35 +0800

    未完成列表可以排序了

--- SmartMemo/src/com/telc/ui/main/viewModel/UnfinishFragment.java
+++ SmartMemo/src/com/telc/ui/main/viewModel/UnfinishFragment.java
@@ -1,6 +1,8 @@
 package com.telc.ui.main.viewModel;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -30,24 +32,26 @@ import android.widget.RatingBar;
 import android.widget.SimpleAdapter;
 import android.widget.SimpleAdapter.ViewBinder;
 
-public class UnfinishFragment extends Fragment{
-	
-	//数据库
-		private SQLiteDatabase db;
-		private TimingService timingService;
-		private RealTimeService realTimeService;
-		private PeriodicService periodicService;
-		private SharedPreferences sp;//用来获取xml保存的useiId
-		
-		ListView uncompleteList;
-//		保存list中的item的列表
-		List<Map <String, Object>> mList=new ArrayList<Map<String,Object>>();
-//		listView适配器
-		SimpleAdapter mAdapter=null;
-//		适配器中的key
-		String[] from={"textListCategory","ratingBarListItem","textListContent"};
-//		value
-		int[] to={R.id.textListCategory,R.id.ratingBarListItem,R.id.textListContent};
+public class UnfinishFragment extends Fragment {
+
+	// 数据库
+	private SQLiteDatabase db;
+	private TimingService timingService;
+	private RealTimeService realTimeService;
+	private PeriodicService periodicService;
+	private SharedPreferences sp;// 用来获取xml保存的useiId
+
+	ListView uncompleteList;
+	// 保存list中的item的列表
+	List<Map<String, Object>> mList = new ArrayList<Map<String, Object>>();
+	// listView适配器
+	SimpleAdapter mAdapter = null;
+	// 适配器中的key
+	String[] from = { "textListCategory", "ratingBarListItem",
+			"textListContent" };
+	// value
+	int[] to = { R.id.textListCategory, R.id.ratingBarListItem,
+			R.id.textListContent };
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -59,126 +63,179 @@ public class UnfinishFragment extends Fragment{
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		View view=inflater.inflate(R.layout.activity_unfinish, null);
-		uncompleteList=(ListView) view.findViewById(R.id.listViewUnfinish);
-		sp=getActivity().getSharedPreferences("Login", getActivity().MODE_PRIVATE);
-		
-		//实例化Adapter
+		View view = inflater.inflate(R.layout.activity_unfinish, null);
+		uncompleteList = (ListView) view.findViewById(R.id.listViewUnfinish);
+		sp = getActivity().getSharedPreferences("Login",
+				getActivity().MODE_PRIVATE);
+
+		// 实例化Adapter
 		initAdapert();
 		if (mAdapter != null)
 			uncompleteList.setAdapter(mAdapter);
-//		listView中Item的监听
+		// listView中Item的监听
 		uncompleteList.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
 			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
 					long arg3) {
 				// TODO Auto-generated method stub
-				
+
 			}
 		});
-		
+
 		return view;
 	}
 
 	private void initAdapert() {
 		// TODO Auto-generated method stub
-		//打开数据库
-		db=getActivity().openOrCreateDatabase(DBConstant.DB_FILENAME,
+		// 打开数据库
+		db = getActivity().openOrCreateDatabase(DBConstant.DB_FILENAME,
 				getActivity().MODE_PRIVATE, null);
-		//实例化数据库服务
-		timingService=new TimingService(db);
-		realTimeService=new RealTimeService(db);
-		periodicService=new PeriodicService(db);
-//		获取userId
-		String userId=sp.getString("user", null);
-//		数据库中获取的List<Timing>
+		// 实例化数据库服务
+		timingService = new TimingService(db);
+		realTimeService = new RealTimeService(db);
+		periodicService = new PeriodicService(db);
+		// 获取userId
+		String userId = sp.getString("user", null);
+		// 数据库中获取的List<Timing>
 		List<Timing> timingList = timingService.getTimingByUserID(userId);
 		List<RealTime> realList = realTimeService.getRealTimeByUserID(userId);
 		List<Periodic> perioList = periodicService.getPeriodicByUserID(userId);
-		
-		if(timingList!=null ){
-		Timing tempTiming;
-//		定时提醒迭代器
-		Iterator itTiming = timingList.iterator();
-		while(itTiming.hasNext()){
-			tempTiming = (Timing) itTiming.next();
-			if(tempTiming.getIsfinish()==0){
-				Map <String, Object> mListItem=new HashMap<String, Object>();
-				String temp;
-				mListItem.put("textListCategory", "定时提醒");
-				mListItem.put("ratingBarListItem", (float)tempTiming.getPriority());
-				if(tempTiming.getContent().length()<=10){
-					temp=tempTiming.getContent();
-				}else{
-					temp=tempTiming.getContent().substring(0, 10)+"……";
+
+		if (timingList != null) {
+			Collections.sort(timingList, new Comparator<Timing>() {
+
+				@Override
+				public int compare(Timing lhs, Timing rhs) {
+					String timeidString = lhs.getTiming_id();
+					String tiString = rhs.getTiming_id();
+					if (lhs.getPriority() < rhs.getPriority()) {
+						return 1;
+					} else if (lhs.getPriority() == rhs.getPriority()) {
+						if (timeidString.compareTo(tiString) > 0) {
+							return 1;
+						} else {
+							return -1;
+						}
+					} else {
+						return -1;
+					}
 				}
-				mListItem.put("textListContent",temp);
+			});
+		}
+		
+		if (perioList != null) {
+		Collections.sort(perioList, new Comparator<Periodic>() {
+
+			@Override
+			public int compare(Periodic lhs, Periodic rhs) {
+				if (lhs.getPriority() > rhs.getPriority()) {
+					return 1;
+				} else {
+					return -1;
+				}
+			}
+		});
+		}
+		
+		if (realList != null) {
+		Collections.sort(realList, new Comparator<RealTime>() {
+
+			@Override
+			public int compare(RealTime lhs, RealTime rhs) {
+				if (lhs.getPriority() > rhs.getPriority()) {
+					return 1;
+				} else {
+					return -1;
+				}
+			}
+		});
+		}
+
+		if (timingList != null) {
+			Timing tempTiming;
+			// 定时提醒迭代器
+			Iterator itTiming = timingList.iterator();
+			while (itTiming.hasNext()) {
+				tempTiming = (Timing) itTiming.next();
+				if (tempTiming.getIsfinish() == 0) {
+					Map<String, Object> mListItem = new HashMap<String, Object>();
+					String temp;
+					mListItem.put("textListCategory", "定时提醒");
+					mListItem.put("ratingBarListItem",
+							(float) tempTiming.getPriority());
+					if (tempTiming.getContent().length() <= 10) {
+						temp = tempTiming.getContent();
+					} else {
+						temp = tempTiming.getContent().substring(0, 10) + "……";
+					}
+					mListItem.put("textListContent", temp);
+					mList.add(mListItem);
+				}
+			}
+		}
+
+		if (realList != null) {
+			// 实时提醒迭代器
+			RealTime tempRealTime;
+			Iterator itRealTime = realList.iterator();
+			while (itRealTime.hasNext()) {
+				tempRealTime = (RealTime) itRealTime.next();
+				if (tempRealTime.getIsfinish() == 0) {
+					Map<String, Object> mListItem = new HashMap<String, Object>();
+					String temp;
+					mListItem.put("textListCategory", "实时提醒");
+					mListItem.put("ratingBarListItem",
+							(float) tempRealTime.getPriority());
+					if (tempRealTime.getContent().length() <= 10) {
+						temp = tempRealTime.getContent();
+					} else {
+						temp = tempRealTime.getContent().substring(0, 10)
+								+ "……";
+					}
+					mListItem.put("textListContent", temp);
+					mList.add(mListItem);
+				}
+			}
+		}
+
+		if (perioList != null) {
+			Periodic tempPreiodic;
+			// 周期性提醒迭代器
+			Iterator itPeriodic = perioList.iterator();
+			while (itPeriodic.hasNext()) {
+				tempPreiodic = (Periodic) itPeriodic.next();
+				Map<String, Object> mListItem = new HashMap<String, Object>();
+				String temp;
+				mListItem.put("textListCategory", "周期性提醒");
+				mListItem.put("ratingBarListItem",
+						(float) tempPreiodic.getPriority());
+				if (tempPreiodic.getContent().length() <= 10) {
+					temp = tempPreiodic.getContent();
+				} else {
+					temp = tempPreiodic.getContent().substring(0, 10) + "……";
+				}
+				mListItem.put("textListContent", temp);
 				mList.add(mListItem);
 			}
 		}
-		}
-		
-		if(realList!=null){
-//		实时提醒迭代器
-		RealTime tempRealTime;
-		Iterator itRealTime=realList.iterator();
-		while(itRealTime.hasNext()){
-			tempRealTime=(RealTime) itRealTime.next();
-			if(tempRealTime.getIsfinish()==0){
-				Map <String, Object> mListItem=new HashMap<String, Object>();
-				String temp;
-				mListItem.put("textListCategory", "实时提醒");
-				mListItem.put("ratingBarListItem", (float)tempRealTime.getPriority());
-				if(tempRealTime.getContent().length()<=10){
-					temp=tempRealTime.getContent();
-				}else{
-					temp=tempRealTime.getContent().substring(0, 10)+"……";
-				}
-				mListItem.put("textListContent",temp);
-				mList.add(mListItem);
-			}
-		}
-		}
-		
-		if(perioList!=null){
-		Periodic tempPreiodic;
-//		周期性提醒迭代器
-		Iterator itPeriodic=perioList.iterator();
-		while(itPeriodic.hasNext()){
-			tempPreiodic=(Periodic) itPeriodic.next();
-			Map <String, Object> mListItem=new HashMap<String, Object>();
-			String temp;
-			mListItem.put("textListCategory", "周期性提醒");
-			mListItem.put("ratingBarListItem", (float)tempPreiodic.getPriority());
-			if(tempPreiodic.getContent().length()<=10){
-				temp=tempPreiodic.getContent();
-			}else{
-				temp=tempPreiodic.getContent().substring(0, 10)+"……";
-			}
-			mListItem.put("textListContent",temp);
-			mList.add(mListItem);
-		}
-		}
-        mAdapter=new SimpleAdapter(getActivity(), mList, R.layout.listview_layout, 
-				from, to);
-		//重写Adapter支持RatingBar
+		mAdapter = new SimpleAdapter(getActivity(), mList,
+				R.layout.listview_layout, from, to);
+		// 重写Adapter支持RatingBar
 		mAdapter.setViewBinder(new ViewBinder() {
 			@Override
 			public boolean setViewValue(View view, Object data,
 					String textRepresentation) {
 				// TODO Auto-generated method stub
-				if(view.getId()==R.id.ratingBarListItem){
-					Float value=(Float) data;
-					float ratingValue=value.floatValue();
-					RatingBar ratingBar=(RatingBar) view;
+				if (view.getId() == R.id.ratingBarListItem) {
+					Float value = (Float) data;
+					float ratingValue = value.floatValue();
+					RatingBar ratingBar = (RatingBar) view;
 					ratingBar.setRating(ratingValue);
 					return true;
-				}else 
-				return false;
+				} else
+					return false;
 			}
 		});
-		}
+	}
 }
-
-
