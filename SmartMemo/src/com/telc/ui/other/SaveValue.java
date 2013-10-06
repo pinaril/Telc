8b5cd4d507a5dc164193d827529fa3755a7a6a4d
hongxukangkang <zhaokangk@sina.com>
2013-10-06 16:07:09 +0800

    “删除设置界面中的多余部分”

--- SmartMemo/src/com/telc/ui/other/SaveValue.java
+++
@@ -1,9 +0,0 @@
-package com.telc.ui.other;
-
-public class SaveValue {
-	static String province = "";
-
-	public static String getProvince() {
-		return province;
-	}
-}
--- SmartMemo/src/com/telc/ui/other/SynchroFragment.java
+++
@@ -1,7 +0,0 @@
-package com.telc.ui.other;
-
-import android.app.Fragment;
-
-public class SynchroFragment extends Fragment {
-
-}
